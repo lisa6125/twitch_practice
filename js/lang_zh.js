eeb44a807c3e464975d60d0a5847952a64ff4489
lisa6125 <fxck6125@gmail.com>
2021-05-26 16:16:11 +0800

    完善語言切換+ search + load more

---
+++ js/lang_zh.js
@@ -0,0 +1,5 @@
+if (!window.I18N) window.I18N = {};
+
+window.I18N["zh"] = {
+  title: "用中文直播的頻道",
+};
--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-const limit = 2;
+let limit = 10;
+let lang = "zh";
+var alldata;
 const clientId = "l8v669ong3mcmzuafjnny8y0tcj9vp";
 const apiUrl = "https://api.twitch.tv/kraken";
 
 let xhr = new XMLHttpRequest();
-xhr.open("GET", `${apiUrl}/streams?limit=10`, true);
-xhr.setRequestHeader("Client-ID", clientId);
-xhr.setRequestHeader("Accept", "application/vnd.twitchtv.v5+json");
-xhr.send();
-
+var init = function () {
+  xhr.open(
+    "GET",
+    `${apiUrl}/streams?limit=` + limit + "&language=" + lang,
+    true
+  );
+  xhr.setRequestHeader("Client-ID", clientId);
+  xhr.setRequestHeader("Accept", "application/vnd.twitchtv.v5+json");
+  xhr.send();
+};
+init();
 xhr.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     let date = JSON.parse(this.responseText);
@@ -18,6 +26,9 @@ xhr.onreadystatechange = function () {
 
 function getdata(data) {
   let res = data.streams;
+  alldata = data.streams;
+  $("#title").text(window.I18N[lang].title);
+  $("#main").html("");
   for (let eachbox of res) {
     $("#main").append(insertcol(eachbox));
   }
@@ -45,4 +56,30 @@ function insertcol(eachbox) {
 function openurl(url) {
   // console.log(url);
   window.open(url, "_blank");
+}
+
+function loadmore() {
+  limit += 5;
+  init();
+  xhr.onreadystatechange();
+}
+function changelang(e) {
+  $("#title").text(window.I18N[e].title);
+  lang = e;
+  init();
+  xhr.onreadystatechange();
+}
+
+function search() {
+  let val = $(".search input").val();
+  $("#main").html("");
+  let filterval = alldata.filter((e) => {
+    return (
+      e.channel.game.indexOf(val) !== -1 ||
+      e.channel.display_name.indexOf(val) !== -1
+    );
+  });
+  for (let eachbox of filterval) {
+    $("#main").append(insertcol(eachbox));
+  }
 }
